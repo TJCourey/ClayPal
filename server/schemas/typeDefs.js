7d814c8314ecd805a65b519f6d96bb8f33b5bca6
Josh Brown <brownjoshalan@gmail.com>
2021-06-28 20:38:04 -0600

    Database is created and typedefs, resolvers are finished

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,53 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    skeetScore: [SkeetScore]!
+    trapScore: [TrapScore]!
+  }
+
+  type SkeetScore {
+    _id: ID
+    overallScore: String
+    station: String
+    weapon: String
+    date: String
+  }
+
+  type TrapScore {
+    _id: ID
+    overallScore: String
+    station: String
+    weapon: String
+    date: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    skeetScore: User
+    trapScore: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addSkeetScore(
+      overallScore: String!
+      station: String!
+      weapon: String!
+    ): User
+    addTrapScore(overallScore: String!, station: String!, weapon: String!): User
+  }
+`;
+
+module.exports = typeDefs;
