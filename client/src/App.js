5891aab5fbafc58c6c1958335e8b1b80194d44ff
jneuner21 <72330999+jneuner21@users.noreply.github.com>
2021-06-30 13:58:39 -0600

    tweaked links and minor things so it all runs

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import Home from "./components/Home.js";
 import MiniDrawer from "./components/NavBar.js";
+import StickyHeadTable from "./pages/LeaderBoard";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import SkeetScore from "./pages/SkeetScore";
+import TrapScore from "./pages/TrapScore";
 
 import skeet from "./components/img/skeet.png";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -7,12 +12,20 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <h1> SKEET SKEET SKEET </h1>
-      <img src={skeet} alt="skeet skeet skeet"></img>
+      {/* <h1> SKEET SKEET SKEET </h1>
+      <img src={skeet} alt="skeet skeet skeet"></img> */}
       <Router>
         <MiniDrawer />
         <Switch>
-          <Route></Route>
+          <Route exact path="/">
+            <Home />{" "}
+          </Route>
+          <Route exact path="/leaderboard">
+            <StickyHeadTable />{" "}
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
         </Switch>
       </Router>
     </div>
