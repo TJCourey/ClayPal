c9f5fee333c1086ccc4b65db7bdf30bc8ceb028d
jneuner21 <72330999+jneuner21@users.noreply.github.com>
2021-06-30 12:34:31 -0600

    build navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
-import StickyFooter from "./components/footer.js";
+import Home from "./components/Home.js";
+import MiniDrawer from "./components/NavBar.js";
+
 import skeet from "./components/img/skeet.png";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -8,10 +10,10 @@ function App() {
       <h1> SKEET SKEET SKEET </h1>
       <img src={skeet} alt="skeet skeet skeet"></img>
       <Router>
+        <MiniDrawer />
         <Switch>
           <Route></Route>
         </Switch>
-        <StickyFooter />
       </Router>
     </div>
   );
