563f71bad9c1e951c6712f48bbf74c250561d524
Thor138 <tjcourey84@gmail.com>
2021-06-30 12:38:05 -0600

    added skeet score tracker page

---
+++ client/src/pages/TrapScore.js
@@ -0,0 +1,27 @@
+const trapRules = [
+  {
+    id: 1,
+    station: 1,
+    maxPoints: 5,
+  },
+  {
+    id: 2,
+    station: 2,
+    maxPoints: 5,
+  },
+  {
+    id: 3,
+    station: 3,
+    maxPoints: 5,
+  },
+  {
+    id: 4,
+    station: 4,
+    maxPoints: 5,
+  },
+  {
+    id: 5,
+    station: 5,
+    maxPoints: 5,
+  },
+];
