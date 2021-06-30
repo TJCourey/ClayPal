1f5ac1bf4ded3d84d7f4e41024a85631ab30b48b
Thor138 <tjcourey84@gmail.com>
2021-06-30 13:36:56 -0600

    added dashboard boiler plate

--- client/src/pages/TrapScore.js
+++ client/src/pages/TrapScore.js
@@ -78,7 +78,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function VerticalTabs() {
+export default function TrapScore() {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
