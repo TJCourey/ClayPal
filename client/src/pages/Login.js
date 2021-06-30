f90823ed5dde3a6ba2135034e9c87f9ea28bd011
Thor138 <tjcourey84@gmail.com>
2021-06-30 14:34:31 -0600

    pre merge save

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,173 +1,173 @@
-// import React, { useState } from "react";
-// import { Avatar, Button, CssBaseline } from "@material-ui/core";
-// import TextField from "@material-ui/core/TextField";
-// import FormControlLabel from "@material-ui/core/FormControlLabel";
-// import Checkbox from "@material-ui/core/Checkbox";
-// // import Link from "@material-ui/core/Link";
-// import Paper from "@material-ui/core/Paper";
-// import Box from "@material-ui/core/Box";
-// import Grid from "@material-ui/core/Grid";
-// import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-// import Typography from "@material-ui/core/Typography";
-// import { makeStyles } from "@material-ui/core/styles";
-// import { Link } from "react-router-dom";
-// // import { useMutation } from "@apollo/client";
-// import { LOGIN_USER } from "../utils/mutations";
+import React, { useState } from "react";
+import { Avatar, Button, CssBaseline } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+// import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
 
-// import Auth from "../utils/auth";
+import Auth from "../utils/auth";
 
-// const Login = (props) => {
-//   const [formState, setFormState] = useState({ email: "", password: "" });
-//   const [login, { error, data }] = useMutation(LOGIN_USER);
+const Login = (props) => {
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
 
-//   // update state based on form input changes
-//   const handleChange = (event) => {
-//     const { name, value } = event.target;
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
 
-//     setFormState({
-//       ...formState,
-//       [name]: value,
-//     });
-//   };
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
 
-//   // submit form
-//   const handleFormSubmit = async (event) => {
-//     event.preventDefault();
-//     console.log(formState);
-//     try {
-//       const { data } = await login({
-//         variables: { ...formState },
-//       });
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
 
-//       Auth.login(data.login.token);
-//     } catch (e) {
-//       console.error(e);
-//     }
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
 
-//     setFormState({
-//       email: "",
-//       password: "",
-//     });
-//   };
+    setFormState({
+      email: "",
+      password: "",
+    });
+  };
 
-//   function Copyright() {
-//     return (
-//       <Typography variant="body2" color="textSecondary" align="center">
-//         {"Copyright © "}
-//         <Link color="inherit" href="https://material-ui.com/">
-//           Your Website
-//         </Link>{" "}
-//         {new Date().getFullYear()}
-//         {"."}
-//       </Typography>
-//     );
-//   }
+  function Copyright() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {"Copyright © "}
+        <Link color="inherit" href="https://material-ui.com/">
+          Your Website
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }
 
-//   const useStyles = makeStyles((theme) => ({
-//     root: {
-//       height: "100vh",
-//     },
-//     image: {
-//       backgroundImage: "url(https://source.unsplash.com/random)",
-//       backgroundRepeat: "no-repeat",
-//       backgroundColor:
-//         theme.palette.type === "light"
-//           ? theme.palette.grey[50]
-//           : theme.palette.grey[900],
-//       backgroundSize: "cover",
-//       backgroundPosition: "center",
-//     },
-//     paper: {
-//       margin: theme.spacing(8, 4),
-//       display: "flex",
-//       flexDirection: "column",
-//       alignItems: "center",
-//     },
-//     avatar: {
-//       margin: theme.spacing(1),
-//       backgroundColor: theme.palette.secondary.main,
-//     },
-//     form: {
-//       width: "100%", // Fix IE 11 issue.
-//       marginTop: theme.spacing(1),
-//     },
-//     submit: {
-//       margin: theme.spacing(3, 0, 2),
-//     },
-//   }));
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      height: "100vh",
+    },
+    image: {
+      backgroundImage: "url(https://source.unsplash.com/random)",
+      backgroundRepeat: "no-repeat",
+      backgroundColor:
+        theme.palette.type === "light"
+          ? theme.palette.grey[50]
+          : theme.palette.grey[900],
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+    },
+    paper: {
+      margin: theme.spacing(8, 4),
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: "100%", // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
 
-//   function SignInSide() {
-//     const classes = useStyles();
+  function SignInSide() {
+    const classes = useStyles();
 
-//     return (
-//       <Grid container component="main" className={classes.root}>
-//         <CssBaseline />
-//         <Grid item xs={false} sm={4} md={7} className={classes.image} />
-//         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-//           <div className={classes.paper}>
-//             <Avatar className={classes.avatar}>
-//               <LockOutlinedIcon />
-//             </Avatar>
-//             <Typography component="h1" variant="h5">
-//               Sign in
-//             </Typography>
-//             <form className={classes.form} noValidate>
-//               <TextField
-//                 variant="outlined"
-//                 margin="normal"
-//                 required
-//                 fullWidth
-//                 id="email"
-//                 label="Email Address"
-//                 name="email"
-//                 autoComplete="email"
-//                 autoFocus
-//               />
-//               <TextField
-//                 variant="outlined"
-//                 margin="normal"
-//                 required
-//                 fullWidth
-//                 name="password"
-//                 label="Password"
-//                 type="password"
-//                 id="password"
-//                 autoComplete="current-password"
-//               />
-//               <FormControlLabel
-//                 control={<Checkbox value="remember" color="primary" />}
-//                 label="Remember me"
-//               />
-//               <Button
-//                 type="submit"
-//                 fullWidth
-//                 variant="contained"
-//                 color="primary"
-//                 className={classes.submit}
-//               >
-//                 Sign In
-//               </Button>
-//               <Grid container>
-//                 <Grid item xs>
-//                   <Link href="#" variant="body2">
-//                     Forgot password?
-//                   </Link>
-//                 </Grid>
-//                 <Grid item>
-//                   <Link href="#" variant="body2">
-//                     {"Don't have an account? Sign Up"}
-//                   </Link>
-//                 </Grid>
-//               </Grid>
-//               <Box mt={5}>
-//                 <Copyright />
-//               </Box>
-//             </form>
-//           </div>
-//         </Grid>
-//       </Grid>
-//     );
-//   }
-// };
+    return (
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <form className={classes.form} noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Box mt={5}>
+                <Copyright />
+              </Box>
+            </form>
+          </div>
+        </Grid>
+      </Grid>
+    );
+  }
+};
 
-// export default Login;
+export default Login;
