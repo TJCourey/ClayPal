e2eaae3545c66480bbd5e4b79e7a5b7eee15f8c9
Nick Strong <nstrong92@yahoo.com>
2021-07-01 10:41:25 -0600

    signup link in login form added

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -161,7 +161,7 @@ const Login = (props) => {
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link to="/signup" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
