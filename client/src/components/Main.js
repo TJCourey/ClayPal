ac2a0f8512ba490c92319b5745b76de7c5912346
Nick Strong <nstrong92@yahoo.com>
2021-07-02 10:04:17 -0600

    added graph to dashboard

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,17 +1,19 @@
 // if logged in render dashboard if not render sign in page
 // dashboard.js and login page should render in return.
 
-// import Dashboard from "../pages/Dashboard";
+import Dashboard from "../pages/Dashboard";
 
 const Main = () => {
   return (
-    <div>
-      <h1> hello world </h1>
-      <h1> hello world </h1>
-      <h1> hello world </h1>
-      <h1> hello world </h1>
-    </div>
-    // <Dashboard />
+    // <div>
+    //   <h1> hello world </h1>
+    //   <h1> hello world </h1>
+    //   <h1> hello world </h1>
+    //   <h1> hello world </h1>
+    // </div>
+    <>
+      <Dashboard />
+    </>
   );
 };
 
