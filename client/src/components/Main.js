f49eb5ed4a7d33b275d0f59bf33bb8bce0373f7a
Thor138 <tjcourey84@gmail.com>
2021-07-02 10:14:56 -0600

    leaderboard is getting info now, main is now a switching point  theoretically

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,17 +1,16 @@
 // if logged in render dashboard if not render sign in page
 // dashboard.js and login page should render in return.
 
+import Dashboard from "../pages/Dashboard";
+import Login from "../pages/Login";
+
 // import Dashboard from "../pages/Dashboard";
 
 const Main = () => {
   return (
-    <div>
-      <h1> hello world </h1>
-      <h1> hello world </h1>
-      <h1> hello world </h1>
-      <h1> hello world </h1>
-    </div>
-    // <Dashboard />
+    <>
+      loggedIn ? <Dashboard /> : <Login />
+    </>
   );
 };
 
