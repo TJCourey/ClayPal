af4e68c40d7db299168813ef11acb68feea7d9b3
Thor138 <tjcourey84@gmail.com>
2021-08-02 16:46:27 -0600

    added config.json

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
+const CONFIG = require("../../config.json");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/ahhskeetskeet",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+console.log(CONFIG);
+
+mongoose.connect(process.env.MONGODB_URI || CONFIG.localMongo, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 module.exports = mongoose.connection;
