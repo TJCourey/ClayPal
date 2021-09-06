c8e9a8cd3eecfbfb20ff0e4add19d17b747b2ae0
Zach Wahrer <zachtheclimber@gmail.com>
2021-09-06 15:09:20 -0600

    Fix malformed connection string for client connecting to backend

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ import { GlobalProvider } from "./utils/GlobalState.js";
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: "/graphql",
+  uri: "http://localhost:3001/graphql",
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
